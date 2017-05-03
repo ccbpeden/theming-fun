fe119d9a9201e9be0570b526b794a08dca8f21f1
Charles Peden and Patrick McGreevy <charles+patrick@>
2017-05-03 11:48:04 -0700

    play some more with custom themes

--- sites/all/themes/fancy/page.tpl.php
+++ sites/all/themes/fancy/page.tpl.php
@@ -1,28 +1,38 @@
-<div id="header">
-  <?php if ($page['page_top']): ?>
-    <?php print render($page['page_top']); ?>
-  <?php endif; ?>
-</div>
-
 <div id="wrapper">
-  <?php if ($page['help']): ?>
-  <?php print render($page['help']); ?>
-<?php endif; ?>
+
+  <div id="header">
+    <a href="<?php print $front_page;?>">
+      <img src="/<?php print $directory;?>/images/logo.png" alt="<?php print $site_name;?>" height="80" width="150" />
+    </a>
+
+    <?php if ($main_menu): ?>
+      <?php print theme('links', $main_menu); ?>
+    <?php endif; ?>
+
+  </div>
+
   <div id="content">
-    <?php if ($page['deals']): ?>
-    <?php print render($page['deals']); ?>
-    <?php endif; ?>
-  </div>
+    <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <div id="sidebar">
-    <?php if ($page['content']): ?>
+    <?php print render($messages); ?>
+    <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
     <?php print render($page['content']); ?>
+  </div>
+
+  <?php if ($page['sidebar_first']): ?>
+    <div id="sidebar">
+      <?php print render($page['sidebar_first']); ?>
+    </div>
+  <?php endif; ?>
+
+  <div id="footer">
+    <?php if ($page['footer']): ?>
+      <?php print render($page['footer']); ?>
     <?php endif; ?>
   </div>
 
-</div>
-<div id="footer">
-  <?php if ($page['page_bottom']): ?>
-    <?php print render($page['page_bottom']); ?>
-  <?php endif; ?>
 </div>
